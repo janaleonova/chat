9c1ad41be6fb687fe81411761da3204f9068c303
janaleonova <93680343+janaleonova@users.noreply.github.com>
2022-03-18 13:44:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,25 @@ function sutitZinu(){
 
 async function ieladetChataZinas(){ 
         let datiNoServera = await fetch(API+'/lasit');
-        let dati =await datiNoServera.text();
-        zinas.innerHTML = dati;
+        let dati =await datiNoServera.json();
+        let teksts="";
+        for(let i=0;i<dati.length;i++){
+                teksts=teksts+"<b>"+dati[i].vards+":</b> "+dati[i].zina+"<br />";
+        zinas.innerHTML = teksts;
 }
- setInterval( ieladetChataZinas, 1000)
+ setInterval( ieladetChataZinas, 1000);
+
+     
+function nosutit(){
+let vards = document.getElementById('vards').value;
+if(vards!=""){
+        if(zina.value!=""){
+        zinas.innerHTML = zinas.innerHTML+"<br /><b>"+
+                        vards+":</b> "+zina.value;
+        zina.value="";
+        }else{
+        alert("Ieraksti ziņu!");
+        }
+}else{
+        alert("Čats nepieļauj anonīmu ziņu iesniegšanu!");
+}
