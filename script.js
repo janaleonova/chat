9dcd3c388e2d31578f1bfc00b9efad42d2ac5e9c
janaleonova <93680343+janaleonova@users.noreply.github.com>
2022-03-18 12:10:06 +0200

    chata paraugs

---
+++ script.js
@@ -0,0 +1,14 @@
+let zina = document.querySelector('.manaZina');
+let zinas = document.querySelector('.chataZinas');
+
+
+function sutitZinu(){
+   zinas.innerHTML = zinas.innerHTML+'<br>'+zina.value;
+}
+
+async function ieladetChataZinas(){ 
+        let datiNoServera = await fetch('chatazinas.txt');
+        let dati =await datiNoServera.text();
+        zinas.innerHTML = dati;
+}
+ setInterval( ieladetChataZinas, 1000)
