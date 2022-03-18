9e74c5e57edddf5cb4dcfadf6b2d60a254a916cc
janaleonova <93680343+janaleonova@users.noreply.github.com>
2022-03-18 15:38:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const API="https://chatpy.janaleonova.repl.co"
 
-/*const API = 'https://chat2022.armandspucs.repl.co'*/
 
 let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
+let vards = document.querySelector('.vards');
 
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
 
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
-   fetch(API+'/sutit/Juris/'+zina.value);
+
+    fetch(API + '/sutit/' +vards.value+'/'+zina.value)
+}
 
 async function ieladetChataZinas()
 {
@@ -19,5 +21,15 @@ async function ieladetChataZinas()
     zinas.innerHTML = dati;
 }
 
-setInterval( ieladetChataZinas, 1000 )
+async function ieladetChataZinasJson()
+{
+    let datiNoServera = await fetch(API + '/lasit');
+    let dati = await datiNoServera.json();
+    i = 0;
+    while (i< await dati.length){
+        console.log(i);
+        i=i+1;
+    }
+}
 
+//setInterval( ieladetChataZinas, 1000 )
